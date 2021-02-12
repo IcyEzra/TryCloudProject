234bc75f6d027a31c1de6e3f5bb57f363b0be179
IcyEzra <uzairbaig.work@gmail.com>
2021-02-11 19:50:57 -0500

    made and fixed changes

--- src/test/java/com/trycloud/tests/base/TestBase.java
+++ src/test/java/com/trycloud/tests/base/TestBase.java
@@ -103,70 +103,14 @@ public abstract class TestBase {
 
 
 
-    }
 
 
-    public void LogIn_2(){
-        driver = WebDriverFactory.getDriver("chrome");
-        driver.manage().window().maximize();
-        driver.get("http://qa3.trycloud.net");
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-        String username = "User59";
-        String password = "Userpass123";
 
 
-        WebElement usernameButton = driver.findElement(By.xpath("//input[@id='user']"));
-        usernameButton.sendKeys(username);
-
-        WebElement passwordButton = driver.findElement(By.xpath("//input[@id='password']"));
-        passwordButton.sendKeys(password);
-
-        WebElement loginButton = driver.findElement(By.xpath("//input[@id='submit-form']"));
-        loginButton.click();
-    }
 
 
-    public void LogIn_3(){
-        driver = WebDriverFactory.getDriver("chrome");
-        driver.manage().window().maximize();
-        driver.get("http://qa3.trycloud.net");
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-        String username = "User89";
-        String password = "Userpass123";
 
 
-        WebElement usernameButton = driver.findElement(By.xpath("//input[@id='user']"));
-        usernameButton.sendKeys(username);
-
-        WebElement passwordButton = driver.findElement(By.xpath("//input[@id='password']"));
-        passwordButton.sendKeys(password);
-
-        WebElement loginButton = driver.findElement(By.xpath("//input[@id='submit-form']"));
-        loginButton.click();
-    }
-
-
-    public void LogIn_4(){
-        driver = WebDriverFactory.getDriver("chrome");
-        driver.manage().window().maximize();
-        driver.get("http://qa3.trycloud.net");
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-        String username = "User119";
-        String password = "Userpass123";
-
-
-        WebElement usernameButton = driver.findElement(By.xpath("//input[@id='user']"));
-        usernameButton.sendKeys(username);
-
-        WebElement passwordButton = driver.findElement(By.xpath("//input[@id='password']"));
-        passwordButton.sendKeys(password);
-
-        WebElement loginButton = driver.findElement(By.xpath("//input[@id='submit-form']"));
-        loginButton.click();
-    }
 
 
 
--- src/test/java/com/trycloud/utilities/Driver.java
+++ src/test/java/com/trycloud/utilities/Driver.java
@@ -9,41 +9,6 @@ import java.util.concurrent.TimeUnit;
 
 public class Driver {
 
-
-    private Driver(){}
-
-    private static WebDriver driver;
-
-    public static WebDriver getDriver(){
-
-        if (driver == null){
-
-            String browser = ConfigurationReader.getProperty("browser");
-
-            switch (browser){
-                case "chrome":
-                    WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
-                    driver.manage().window().maximize();
-                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-                    break;
-                case "firefox":
-                    WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver();
-                    driver.manage().window().maximize();
-                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-                    break;
-                default:
-                    WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
-                    driver.manage().window().maximize();
-                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-                    break;
-            }
-        }
-
-
-
     private Driver() {
     }
 
@@ -97,19 +62,28 @@ public class Driver {
             //This same driver will be returned every time we call Driver.getDriver() method
 
         }
-
         return driver;
 
     }
 
 
-
     public static void closeDriver () {
         if (driver != null) {
-
             driver.quit();
             driver = null;
         }
     }
 
+
+
+
+
+
+
+
+
+
+
 }
+
+
